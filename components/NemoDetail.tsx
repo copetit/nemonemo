da620d19a9775bbb16eba81130ae01495de6c0b0
han <copetit412@gmail.com>
2022-05-18 22:44:03 +0900

    modify design

--- components/NemoDetail.tsx
+++ components/NemoDetail.tsx
@@ -3,17 +3,16 @@ export default function NemoDetail(props: any) {
   const nemoDate = new Date(nemoDetail.createAt);
   return (
     <div className="p-5 h-32 flex justify-center items-center">
-      <div className="relative w-2/3 h-full bg-neutral-200 rounded-md overflow-hidden">
-        {nemoDetail ? (
-          <div
-            className="absolute w-3/12 h-full top-0 left-0 flex items-center justify-center p-3"
-            style={{
-              backgroundColor: nemoDetail ? nemoDetail.color : "#c3c3c3",
-            }}
-          >
-            {nemoDate.toDateString()}
-          </div>
-        ) : null}
+      <div className="w-2/3 h-full flex bg-neutral-200 rounded-md overflow-hidden">
+        <div
+          className="w-3/12 flex items-center justify-center p-3"
+          style={{
+            backgroundColor: nemoDetail ? nemoDetail.color : "#c3c3c3",
+          }}
+        >
+          {nemoDate.toDateString()}
+        </div>
+        <div className="p-3">{nemoDetail.memo}</div>
       </div>
     </div>
   );
