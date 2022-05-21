1c0ad83e10ea0f328269c8257fd5af5bc23e1a63
han <copetit412@gmail.com>
2022-05-21 18:26:12 +0900

    add logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -85,23 +85,30 @@ const Home: NextPage = () => {
   // get Data 1 month
   return (
     <div className="mx-auto w-full max-w-2xl">
+      <div className="px-5 pt-5 w-20">
+        <a href="/">
+          <img src="/Nemo.svg" alt="logo" />
+        </a>
+      </div>
       <div className="p-5">
-        <button onClick={gridGapHandler}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"
-            />
-          </svg>
-        </button>
+        <div className="flex justify-end my-2 hover:text-amber-500 transition-colors">
+          <button className="" onClick={gridGapHandler}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"
+              />
+            </svg>
+          </button>
+        </div>
         {color && nemoDatas && (
           <div
             className={`grid grid-cols-8 sm:grid-cols-10  md:grid-cols-10 lg:grid-cols-14 ${
@@ -164,7 +171,7 @@ const Home: NextPage = () => {
               </div>
             </div>
             <button
-              className="px-5 py-3 rounded-lg flex justify-center w-full sm:w-1/2 mx-auto my-3 bg-gradient-to-r transition-transform from-green-400 to-blue-500 shadow-md  text-white hover:scale-105 tracking-wider"
+              className="px-5 py-3 rounded-lg flex justify-center w-full sm:w-1/2 mx-auto my-3 bg-amber-500 text-white transition-transform hover:scale-105 tracking-wider shadow-md"
               type="submit"
               onClick={() => setValue("color", color.hex)}
             >
